30dcdfe4fc78d7cf213886dc97d2c0b2e15897eb
masa-02 <msykbys02@gmail.com>
2026-06-17 14:37:35 +0900

    Initial Commit, modfy for own env.

--- scripts/find_heads.sh
+++ scripts/find_heads.sh
@@ -1,6 +1,6 @@
 MODELS=( "granite3_8b-attn" "qwen2-attn" "llama3_8b-attn" "phi3-attn" "mistral_7b-attn" "gemma2_9b-attn" )
 for MODEL in "${MODELS[@]}"; do
-    python3 -u select_head.py \
+    uv run python3 -u select_head.py \
                             --model_name ${MODEL} \
                             --num_data 30 \
                             --dataset llm  >> "analysis.txt"
--- scripts/run_dataset.sh
+++ scripts/run_dataset.sh
@@ -2,7 +2,7 @@ MODELS=( "granite3_8b-attn" "llama3_8b-attn" "mistral_7b-attn" "gemma2_9b-attn" 
 dataset="deepset"
 
 for MODEL in "${MODELS[@]}"; do
-    python run_dataset.py \
+    uv run python run_dataset.py \
                     --model_name ${MODEL} \
                     --seed 0
 done
